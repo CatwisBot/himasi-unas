fa2b7c5237b2ffc4afc3ec8702de0372c8a6465c
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-13 17:05:45 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
     return(
         <main>
-            <div className="flex flex-col gap-4 items-center justify-center">
-                <h2 className="font-bold text-5xl text-black"> Raden Whisnu Arya Nugraha </h2>
-            </div>
+            
         </main>
     )
 }
