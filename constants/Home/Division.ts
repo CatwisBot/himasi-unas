d6916c650ce8a33fd2f4c35378b929f29db90967
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-15 01:37:49 +0700

    add data division

---
+++ constants/Home/Division.ts
@@ -0,0 +1,30 @@
+import { Division } from "@/lib/type/tDivision";
+import Laptop from "@/public/icon/Home/Laptop.png";
+
+export const divisions: Division[] = [
+  {
+    id: 1,
+    title: "Research and Development",
+    image: Laptop,
+  },
+  {
+    id: 2,
+    title: "Creative Media",
+    image: Laptop,
+  },
+  {
+    id: 3,
+    title: "Public Relation",
+    image: Laptop,
+  },
+  {
+    id: 4,
+    title: "Entrepreneurship",
+    image: Laptop,
+  },
+  {
+    id: 5,
+    title: "Human Talent Development",
+    image: Laptop,
+  },
+];
