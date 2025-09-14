06393f170d04f185974f87ae3ecab7653811b3a4
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-14 23:25:46 +0700

    add anggota and divisi

--- components/shared/Home/Introduction.tsx
+++ components/shared/Home/Introduction.tsx
@@ -18,10 +18,14 @@ export default function Introduction() {
             </div>
            </div>
 
-           <div className="w-full max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto px-8 md:px-4 text-justify md:text-center text-white text-xs md:text-sm lg:text-base xl:text-lg leading-relaxed pt-4 pb-12 font-bold">
+           <div className="w-full max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto px-8 md:px-4 text-justify md:text-center text-white text-xs md:text-sm lg:text-base xl:text-lg leading-relaxed pt-4 pb-8 md:pb-12 font-bold">
             <p className="hidden lg:block">Selamat datang di Website HIMASI, Himpunan Mahasiswa Sistem Informasi!<br />Organisasi mahasiswa yang berdedikasi untuk mengembangkan potensi akademik, profesional, dan sosial di<br />bidang Sistem Informasi. Dengan berbagai kegiatan seperti seminar, pelatihan, hingga proyek kolaboratif, kami<br />bertujuan untuk menciptakan lingkungan belajar yang inspiratif sekaligus membangun jejaring yang solid di<br />antara mahasiswa.</p>
             <p className="lg:hidden" style={{ textAlignLast: "center" }}>Selamat datang di Website HIMASI, Himpunan Mahasiswa Sistem Informasi! Organisasi mahasiswa yang berdedikasi untuk mengembangkan potensi akademik, profesional, dan sosial di bidang Sistem Informasi. Dengan berbagai kegiatan seperti seminar, pelatihan, hingga proyek kolaboratif, kami bertujuan untuk menciptakan lingkungan belajar yang inspiratif sekaligus membangun jejaring yang solid di antara mahasiswa.</p>
            </div>
+
+          <div className="flex justify-center items-center pb-6">
+            <p className="text-xs md:text-sm lg:text-base xl:text-lg text-white font-bold whitespace-pre">34 Anggota   ━━━   5 Divisi</p>
+          </div>
         </main>
     )
 }
