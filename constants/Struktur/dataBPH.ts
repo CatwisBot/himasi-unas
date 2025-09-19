5cc3b21a28afcf3210e85b9d9a6b84963bd42c10
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-20 02:10:48 +0700

    feat: add leaders data for organizational structure and remove outdated Kahim data

---
+++ constants/Struktur/dataBPH.ts
@@ -0,0 +1,40 @@
+import { Leader } from "@/lib/type/Struktur/tDataBPH";
+
+export const leaders: Leader[] = [
+  {
+    id: "ketua-1",
+    name: "Omar Nur Rahmatsyah",
+    image: "/image/Struktur/Deadpool.png",
+    role: "Ketua Himpunan",
+    instagram: "https://instagram.com/",
+    description:
+      "Sebagai Ketua Himpunan, bertanggung jawab dalam memimpin organisasi, mengarahkan strategi, serta menjaga koordinasi antar divisi.",
+  },
+  {
+    id: "wakil-1",
+    name: "Muhammad Fahreza Situmorang",
+    image: "/image/Struktur/Deadpool.png",
+    role: "Wakil Himpunan",
+    instagram: "https://instagram.com/",
+    description:
+      "Sebagai Wakil Himpunan, mendampingi ketua dalam pengambilan keputusan, memastikan program berjalan, serta menjadi penghubung antar anggota.",
+  },
+  {
+    id: "bendahara-1",
+    name: "Kyla Nazwara Sofyan",
+    image: "/image/Struktur/Deadpool.png",
+    role: "Bendahara",
+    instagram: "https://instagram.com/",
+    description:
+      "Sebagai Wakil Himpunan, mendampingi ketua dalam pengambilan keputusan, memastikan program berjalan, serta menjadi penghubung antar anggota.",
+  },
+  {
+    id: "sekretaris-1",
+    name: "Linda Isnaeni",
+    image: "/image/Struktur/Deadpool.png",
+    role: "Sekretaris",
+    instagram: "https://instagram.com/",
+    description:
+      "Sebagai Wakil Himpunan, mendampingi ketua dalam pengambilan keputusan, memastikan program berjalan, serta menjadi penghubung antar anggota.",
+  },
+]
