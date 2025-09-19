ceaab31f139fe65959344a91440f8f898d5c414d
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-20 02:11:02 +0700

    feat: define Leader type with properties for organizational structure

--- lib/type/Struktur/tDataBPH.ts
+++ lib/type/Struktur/tDataBPH.ts
@@ -2,7 +2,7 @@ export type Leader = {
   id: string
   name: string
   image: string
-  role: "Ketua Himpunan" | "Wakil Himpunan"
+  role: "Ketua Himpunan" | "Wakil Himpunan" | "Bendahara" | "Sekretaris"
   instagram: string
   description: string
 }
