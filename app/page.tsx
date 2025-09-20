1814a5380620bce1f7d6e56956cacb70f942da75
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-21 02:39:39 +0700

    fix: add Slide component to Home page for enhanced visual presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/shared/Home/Hero";
 import Introduction from "@/components/shared/Home/Introduction";
+import Slide from "@/components/shared/Home/SlideImage";
 
 export default function Home() {
     return(
         <main>
             <Hero />
             <Introduction />
+            <Slide />
         </main>
     )
 }
