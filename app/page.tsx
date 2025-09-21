d175655198933ad01e775ecb762735b04eb77443
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-21 23:45:21 +0700

    refactor: clean up Home component structure for better readability

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Hero from "@/components/shared/Home/Hero";
 import Introduction from "@/components/shared/Home/Introduction";
 import Slide from "@/components/shared/Home/SlideImage";
+import VM from "@/components/shared/Home/VM";
+import Kegiatan from "@/components/shared/Home/Activity";
 
 export default function Home() {
     return(
-        <main>
+        <main className="overflow-hidden">
             <Hero />
             <Introduction />
             <Slide />
+            <VM />
+            <Kegiatan />
         </main>
     )
 }
