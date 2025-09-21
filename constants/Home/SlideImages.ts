d927e5399a5b94d2dbdcb39faa2318c70125eba6
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-21 16:23:44 +0700

    feat: add SlideImages data for enhanced visual presentation in Home component

---
+++ constants/Home/SlideImages.ts
@@ -0,0 +1,47 @@
+import KahimImage from "@/public/image/Home/Kahim.png";
+import { SlideImageData } from "@/lib/type/Home/tSlideImage";
+
+export const slideImages: SlideImageData[] = [
+  {
+    id: 1,
+    src: KahimImage,
+    alt: "HIMASI Keren",
+    backgroundColor: "#ffffffff",
+    textColor: "white",
+  },
+  {
+    id: 2,
+    src: KahimImage,
+    alt: "HIMASI Hebat",
+    backgroundColor: "#ffffffff",
+    textColor: "white",
+  },
+  {
+    id: 3,
+    src: KahimImage,
+    alt: "HIMASI Kuat",
+    backgroundColor: "#ffffffff",
+    textColor: "black",
+  },
+  {
+    id: 4,
+    src: KahimImage,
+    alt: "HIMASI Kreatif",
+    backgroundColor: "#ffffffff",
+    textColor: "black",
+  },
+  {
+    id: 5,
+    src: KahimImage,
+    alt: "HIMASI Ramah",
+    backgroundColor: "#ffffffff",
+    textColor: "white",
+  },
+  {
+    id: 6,
+    src: KahimImage,
+    alt: "HIMASI UNAS",
+    backgroundColor: "#ffffffff",
+    textColor: "white",
+  },
+];
