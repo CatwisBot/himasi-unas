252d15de1a1a4dd403947abbd2d3e5862ba5f7e7
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-22 15:25:03 +0700

    feat: add FilteredGalleryItem interface and GalleryFilter type for enhanced gallery management

--- lib/type/Galeri/Galeri.ts
+++ lib/type/Galeri/Galeri.ts
@@ -10,3 +10,31 @@ export interface GalleryEvent {
   desc: string;
   images: GalleryImage[];
 }
+
+export interface FilteredGalleryItem {
+  id: string;
+  category:
+    | "Proker 1"
+    | "Proker 2"
+    | "Proker 3"
+    | "Proker 4"
+    | "Proker 5"
+    | "Himpunan"
+    | "Wawancara";
+  title: string;
+  tanggal: string;
+  lokasi: string;
+  alamatLengkap: string;
+  image: string;
+  alt: string;
+}
+
+export type GalleryFilter =
+  | "All"
+  | "Proker 1"
+  | "Proker 2"
+  | "Proker 3"
+  | "Proker 4"
+  | "Proker 5"
+  | "Himpunan"
+  | "Wawancara";
