f9a4a93e36ece1c2f76f14c31ba1c633a84c0cbf
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-22 15:28:44 +0700

    refactor: remove unused useEffect import from infoBPH component

--- components/shared/Struktur/infoBPH.tsx
+++ components/shared/Struktur/infoBPH.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { leaders } from "@/constants/Struktur/dataBPH";
 import { Instagram } from "lucide-react";
 import type { StaticImageData } from "next/image";
