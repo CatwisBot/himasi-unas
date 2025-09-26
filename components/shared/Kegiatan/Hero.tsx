3c9f14ce6840f9dbbb96c9fd9ed9c39b53b497e5
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-26 17:43:47 +0700

    feat: add HeroKegiatan component for event series display

---
+++ components/shared/Kegiatan/Hero.tsx
@@ -0,0 +1,9 @@
+export default function HeroKegiatan() {
+    return (
+        <main className="bg-[#FFE8DB]">
+            <div className="flex justify-center text-center pt-5 md:pt-10 max-w-4xl mx-auto px-4">
+                <h2 className="uppercase text-lg md:text-4xl font-bold text-black">rangkaian kegiatan himpunan mahasiswa sistem informasi</h2>
+            </div>
+        </main>
+    )
+}
