acd9d0f9899b1ed9b6a3d796d1ee7d49f9073f4d
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-26 17:43:35 +0700

    style: adjust text size in GaleriPage and clean up formatting

--- app/struktur/page.tsx
+++ app/struktur/page.tsx
@@ -2,22 +2,19 @@ import BPH from "@/components/shared/Struktur/infoBPH";
 import Divisi from "@/components/shared/Struktur/infoDivision";
 
 export default function GaleriPage() {
-    return(
+  return (
     <main>
       <div className="bg-[#FFE8DB]">
-        <div className="flex justify-center font-bold text-center text-xl md:text-4xl uppercase py-10 max-w-2xl mx-auto text-black px-4">
+        <div className="flex justify-center font-bold text-center text-lg md:text-4xl uppercase py-10 max-w-2xl mx-auto text-black px-4">
           <h2>Anggota Himpunan Mahasiswa Sistem Informasi</h2>
         </div>
       </div>
-      <div className="bg-[linear-gradient(to_bottom,#FFE8DB_70%,#E4C6BE_80%,#994555_85%,#732E39_90%,#4B061A_100%)] min-h-50">
-
-      </div>
+      <div className="bg-[linear-gradient(to_bottom,#FFE8DB_70%,#E4C6BE_80%,#994555_85%,#732E39_90%,#4B061A_100%)] min-h-50"></div>
       <div className="bg-[#4B061A]">
         <BPH />
       </div>
 
       <Divisi />
-
     </main>
-    );
+  );
 }
