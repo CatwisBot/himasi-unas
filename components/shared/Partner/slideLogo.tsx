dab97492dca308ec59d0bb0ef0cd04742dfb97bf
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-26 20:55:42 +0700

    fix: adjust minimum height of SlideLogo component for better layout

--- components/shared/Partner/slideLogo.tsx
+++ components/shared/Partner/slideLogo.tsx
@@ -1,6 +1,6 @@
 export default function SlideLogo() {
     return(
-        <main className="bg-[#4B061A] min-h-50">
+        <main className="bg-[#4B061A] min-h-40">
             <div>
 
             </div>
