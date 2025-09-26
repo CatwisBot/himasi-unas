af3d3c53d5bdfba8d6182571793befde866e1891
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-26 17:43:52 +0700

    feat: create Pendaftaran component with gradient background

---
+++ components/shared/Kegiatan/Pendaftaran.tsx
@@ -0,0 +1,9 @@
+export default function Pendaftaran() {
+    return(
+        <main className="bg-[linear-gradient(to_bottom,#FFE8DB_70%,#E4C6BE_80%,#994555_85%,#732E39_90%,#4B061A_100%)] min-h-50">
+            <div>
+                
+            </div>
+        </main>
+    )
+}
