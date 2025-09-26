276f29a9225db0204b7e4ce959044b3dfa17d9cf
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-26 17:43:42 +0700

    feat: add Artikel component with layout structure

---
+++ components/shared/Kegiatan/Artikel.tsx
@@ -0,0 +1,10 @@
+export default function Artikel() {
+    return(
+        <main className="bg-[#4B061A] min-h-50">
+            <div className="border-t-5 border-white max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl mx-auto rounded-lg" />
+            <div>
+
+            </div>
+        </main>
+    )
+}
